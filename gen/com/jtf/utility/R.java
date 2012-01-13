1319445edceee95f7a0695ea0365c37208036f9f
Talukder Jabed <jabed.talukder@gmail.com>
2012-01-14 02:11:29 +0900

    Clean up

--- gen/com/jtf/utility/R.java
+++ gen/com/jtf/utility/R.java
@@ -14,6 +14,7 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int btnShow=0x7f050001;
         public static final int tv=0x7f050000;
     }
     public static final class layout {
@@ -22,5 +23,6 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int show=0x7f040002;
     }
 }
